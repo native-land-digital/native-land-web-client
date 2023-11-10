bb4a322a32991a6e4c58f239d95b862f740a4d58
noi5e <will.gutierrez@gmail.com>
2023-11-10 14:28:00 -0800

    implement material-UI grid 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 // import "./App.css";
+import Grid from "@mui/material/Unstable_Grid2";
 
 import FrontPageMap from "./FrontPageMap";
 import NavBar from "./NavBar";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <FrontPageMap />
-    </>
+    <Grid container>
+      <Grid xs={12}>
+        <NavBar />
+      </Grid>
+      <Grid xs={12}>
+        <FrontPageMap />
+      </Grid>
+    </Grid>
   );
 }
 
