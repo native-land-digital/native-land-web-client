62b056cea21b589a8f17a2ef0755d4240725a59e
noi5e <will.gutierrez@gmail.com>
2023-11-10 14:29:12 -0800

    create NavBar component

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,3 +1,9 @@
+import "./NavBar.css";
+
 export default function NavBar() {
-  return <h1>Native Land Digital</h1>;
+  return (
+    <nav id="nav-bar-main" role="navigation" aria-label="Main">
+      <h1 id="title-nav-bar">Native Land Digital</h1>
+    </nav>
+  );
 }
