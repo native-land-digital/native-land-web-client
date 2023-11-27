c9d05116dd0edb88ba06e8700e7b53953ca26e65
noi5e <will.gutierrez@gmail.com>
2023-11-27 11:38:27 -0800

    use Material-UI styled() utility to style List component

--- src/MapLegend.tsx
+++ src/MapLegend.tsx
@@ -1,5 +1,6 @@
 import {
   List,
+  ListProps,
   ListItemButton,
   ListItemText,
   ListSubheader,
@@ -8,69 +9,54 @@ import {
 import { Link } from "react-router-dom";
 
 export default function MapLegend({
+  navBarHeight,
   selectedFeatures,
 }: {
+  navBarHeight: string;
   selectedFeatures: { name: string; id: string | number; slug: string }[] | [];
 }) {
   const featureLinks =
     selectedFeatures.length > 0
       ? selectedFeatures.map((feature, index) => (
-          <ListItemButton key={index} sx={{ cursor: "pointer" }} divider>
+          <ListItemButton key={index} divider>
             <Link to={`/features/${feature.slug}`} style={{ width: "100%" }}>
               <ListItemText
                 primary={feature.name}
-                sx={{ color: "common.white" }}
                 primaryTypographyProps={{ style: { fontSize: "1.7rem" } }}
-                // inset
               />
             </Link>
           </ListItemButton>
         ))
       : [];
 
-  const isDisplayed = selectedFeatures.length > 0 ? "block" : "none";
-
-  // const FeaturesList = styled(List)({
-  //   bgcolor: "info.dark",
-  //   position: "absolute",
-  //   top: "1.5rem",
-  //   left: "1.5rem",
-  //   zIndex: "modal",
-  //   width: "30rem",
-  //   "& .MuiListItemText-root": {
-  //     color: "common.white"
-  //   },
-  //   "& .MuiListSubheader-root": {
-  //     bgcolor: "grey.800",
-  //     color: "info.light",
-  //     fontSize: "1.7rem",
-  //   }
-  // });
+  const FeaturesList = styled(List)<ListProps>(({ theme }) => ({
+    backgroundColor: theme.palette.info.dark,
+    cursor: "pointer",
+    disablePadding: true,
+    position: "absolute",
+    top: `calc(${navBarHeight} + 1.5rem)`,
+    left: "1.5rem",
+    width: "30rem",
+    "& .MuiListItemText-root": {
+      color: theme.palette.common.white,
+    },
+    "& .MuiListSubheader-root": {
+      backgroundColor: theme.palette.grey[800],
+      color: theme.palette.info.light,
+      fontSize: "1.7rem",
+    },
+  }));
 
   return (
-    <List
+    <FeaturesList
       sx={{
-        display: isDisplayed,
-        bgcolor: "info.dark",
-        position: "absolute",
-        top: "1.5rem",
-        left: "1.5rem",
+        display: selectedFeatures.length > 0 ? "block" : "none",
         zIndex: "modal",
-        width: "30rem",
       }}
-      component="nav"
       disablePadding
     >
-      <ListSubheader
-        sx={{
-          bgcolor: "grey.800",
-          color: "info.light",
-          fontSize: "1.7rem",
-        }}
-      >
-        Selected Features
-      </ListSubheader>
+      <ListSubheader>Selected Features</ListSubheader>
       {featureLinks}
-    </List>
+    </FeaturesList>
   );
 }
