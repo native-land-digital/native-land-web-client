3075a6240650cf4d08f57562341dd7566ad716f1
noi5e <will.gutierrez@gmail.com>
2023-12-04 19:25:59 -0800

    use Material UI theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import Feature from "./Feature.tsx";
 import { loader as featureLoader } from "./loaders/features.ts";
 import NavBar from "./NavBar.tsx";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
 // fonts for material UI
@@ -14,6 +15,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { grey } from "@mui/material/colors";
+
+const defaultTheme = createTheme({
+  palette: { background: { default: grey[700] } },
+});
 
 const navBarHeight = "5rem"; // used for dynamic CSS calculation: FrontPageMap's height is 100vh - navBarHeight.
 
@@ -32,8 +38,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CssBaseline enableColorScheme />
-    <NavBar navBarHeight={navBarHeight} />
-    <RouterProvider router={router} />
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline enableColorScheme />
+      <NavBar navBarHeight={navBarHeight} />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
