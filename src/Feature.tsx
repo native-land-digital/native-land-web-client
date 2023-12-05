7907e532440e8fae75ff2d2c875a3d6abf117d00
noi5e <will.gutierrez@gmail.com>
2023-12-04 19:25:42 -0800

    create content container, title header, info chip

--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,17 +1,60 @@
+import { useLoaderData } from "react-router-dom";
+
+import Chip from "@mui/material/Chip";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
-import type { Feature } from "geojson";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
-import { useLoaderData } from "react-router-dom";
+import LandscapeIcon from "@mui/icons-material/Landscape";
+
+import { grey } from "@mui/material/colors";
+
+const getChipLabel = (category: string) => {
+  switch (category) {
+    case "territories":
+      return "Territory";
+
+    case "languages":
+      return "Language";
+
+    case "treaties":
+      return "Treaty";
+  }
+};
 
 export default function Feature() {
   const { feature } = useLoaderData() as { feature: NativeLandFeature };
 
-  console.log(feature);
+  const chipLabel = getChipLabel(feature.category);
 
   return (
     <Container component="main" maxWidth="lg">
-      <Paper variant="outlined"></Paper>
+      <Paper
+        elevation={5}
+        sx={{ bgcolor: grey[800], px: 2, py: 3, mt: "2rem" }}
+        square
+      >
+        <Typography
+          component="h1"
+          variant="h4"
+          sx={{
+            color: "primary.contrastText",
+            fontStyle: "italic",
+            ml: "1rem",
+          }}
+          gutterBottom
+        >
+          {feature.name}
+        </Typography>
+        <Chip
+          color="primary"
+          icon={<LandscapeIcon />}
+          sx={{ mb: "1rem", ml: "1rem", px: "1rem" }}
+          label={chipLabel}
+        />
+        <Divider variant="middle" />
+      </Paper>
     </Container>
   );
 }
