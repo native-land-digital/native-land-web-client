abd9b60c495eb58fcfba6ad6ab2bc4b13c98a8be
noi5e <will.gutierrez@gmail.com>
2023-12-04 19:26:37 -0800

    fix typo to add treaties category

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="vite/client" />
 
-// features returned as JSON from server from API request
+// features returned as JSON from API request to server
 // eg. http://native-land.ca/features/oceti-sakowin-sioux
 interface NativeLandFeature {
   name: string;
   polygon: Feature;
   polygon_style: null;
   pronunciation: null;
-  category: "territories" | "languages" | "territories";
+  category: "territories" | "languages" | "treaties";
   official_websites: null;
   created_at: Date;
   last_modified_at: Date;
