139de479e4699ddbf9aeeea7464e016473247f1a
noi5e <will.gutierrez@gmail.com>
2023-12-05 22:00:59 -0800

    zoom to Feature boundingBox onMapLoad

--- src/Feature.tsx
+++ src/Feature.tsx
@@ -1,10 +1,13 @@
+import { useRef } from "react";
 import { useLoaderData } from "react-router-dom";
+import { Map, MapRef, Layer, Source } from "react-map-gl";
+import bbox from "@turf/bbox";
+import { decode } from "html-entities";
 
 import Chip from "@mui/material/Chip";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 
 import LandscapeIcon from "@mui/icons-material/Landscape";
 
@@ -24,15 +27,31 @@ const getChipLabel = (category: string) => {
 };
 
 export default function Feature() {
-  const { feature } = useLoaderData() as { feature: NativeLandFeature };
+  const {
+    feature: { polygon, category, name },
+  } = useLoaderData() as { feature: NativeLandFeature };
 
-  const chipLabel = getChipLabel(feature.category);
+  const mapRef = useRef<MapRef>(null);
+
+  const zoomToBounds = () => {
+    const [minLng, minLat, maxLng, maxLat] = bbox(polygon);
+
+    mapRef?.current?.fitBounds(
+      [
+        [minLng, minLat],
+        [maxLng, maxLat],
+      ],
+      { padding: 40, duration: 1000 }
+    );
+  };
+
+  const chipLabel = getChipLabel(category);
 
   return (
     <Container component="main" maxWidth="lg">
       <Paper
         elevation={5}
-        sx={{ bgcolor: grey[800], px: 2, py: 3, mt: "2rem" }}
+        sx={{ bgcolor: grey[800], px: 2, py: 3, my: "2rem" }}
         square
       >
         <Typography
@@ -45,7 +64,7 @@ export default function Feature() {
           }}
           gutterBottom
         >
-          {feature.name}
+          {decode(name)}
         </Typography>
         <Chip
           color="primary"
@@ -53,7 +72,22 @@ export default function Feature() {
           sx={{ mb: "1rem", ml: "1rem", px: "1rem" }}
           label={chipLabel}
         />
-        <Divider variant="middle" />
+        <Map
+          mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/mapbox/light-v11"
+          onLoad={zoomToBounds}
+          ref={mapRef}
+          style={{ width: "400px", height: "400px" }}
+          initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 14,
+          }}
+        >
+          <Source type="geojson" data={polygon}>
+            <Layer id="feature-polygon" type="fill" />
+          </Source>
+        </Map>
       </Paper>
     </Container>
   );
