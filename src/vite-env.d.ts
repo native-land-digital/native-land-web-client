c5067bc221deb4e52fb2d60c059c63c10af4f6c1
noi5e <will.gutierrez@gmail.com>
2023-12-10 19:02:52 -0800

    update type definitions for Feature component

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,12 +5,31 @@
 interface NativeLandFeature {
   name: string;
   polygon: Feature;
-  polygon_style: Record<color<string>>;
+  polygon_style: FeaturePolygonStyle;
   pronunciation: null;
   category: "territories" | "languages" | "treaties";
   official_websites: null;
   created_at: Date;
   last_modified_at: Date;
   related: null;
-  content: null;
+  sources: string[];
+  changelog: FeatureChange[];
+}
+
+// date and string pair describing content changes made by admin to individual <Feature> pages
+interface FeatureChange {
+  changeText: string; // sentence describing change
+  changeDate: string;
+}
+
+// anchorLinkText and URL pairs for individual <Feature> pages, linking to outside web content
+interface FeatureOfficialWebsite {
+  webSiteTitle: string; // the descriptive text within the <a> tag
+  webSiteUrl: string;
+}
+
+// object describing polygon style properties in MapBox
+// creates consistency between <FrontPageMap> and <Feature>
+interface FeaturePolygonStyle {
+  color: string; // HTML hex color
 }
