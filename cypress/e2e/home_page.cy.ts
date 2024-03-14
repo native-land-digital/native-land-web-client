75641e5c576540a13b395a6cbd4e49a1ad1e7b0a
noi5e <will.gutierrez@gmail.com>
2024-03-14 16:51:50 -0700

    add test for front page map loading

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -2,4 +2,9 @@ describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("http://localhost:5173");
   });
+
+  it("should load the front page map", () => {
+    const map = cy.get(".mapboxgl-map");
+    map.should("exist");
+  });
 });
